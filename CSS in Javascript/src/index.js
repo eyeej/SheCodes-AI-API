983781c519db67857ff69d3083ebf8aa486b8225
eyeej <eyee.joy@gmail.com>
2025-07-22 09:52:42 +1000

    javascript in css

--- CSS in Javascript/src/index.js
+++ CSS in Javascript/src/index.js
@@ -1,7 +1,12 @@
 // Challenge one
+const button = document.querySelector("button");
 
+button.setAttribute("name", "challenge-1");
+button.setAttribute("disabled", "");
 
 // Challenge two
+let heading = documentquerySelector("h3");
+heading.style.backgroundColor = "green";
 
 
 //Challenge three
