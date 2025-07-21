79246dd04b131d499ce5fefb61036e30fe08d22f
eyeej <eyee.joy@gmail.com>
2025-07-21 15:13:35 +1000

    user errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function ShowerAnswer(reponse) {
+function ShowAnswer(response) {
     console.log(processed);
     alert(response.data.answer);
 }
@@ -6,7 +6,7 @@ function ShowerAnswer(reponse) {
 let apiKey = "t9e9139a19b801fbcfa020d17a47ob1f";
 let context = "Please tell me who was the first female president";
 let prompt = "Who was the first female president and detail the dates";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 console.log("Processing");
 axios.get(apiUrl).then(displayAnswer);
