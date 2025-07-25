4ad350f6972be5f667cc21783b23a97896e13459
eyeej <eyee.joy@gmail.com>
2025-07-25 12:42:22 +1000

    html updates

--- CSS in Javascript/src/index.js
+++ CSS in Javascript/src/index.js
@@ -1,9 +1,10 @@
 function siteSearch(response) {
     preventDefault();
+    console.log();
 }
 
 let apiKey = "t9e9139a19b801fbcfa020d17a47ob1f";
 let prompt = "enter current date";
 let context = "Please provide real time news on artificial intelligence";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-axios.get(apiKey).thenDisplay();
+axios.get(apiKey).then(showResponse);
